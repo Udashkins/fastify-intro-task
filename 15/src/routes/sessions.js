7c579325cae205cf0494ab7be5841d4bd017abdc
Udashkins <king_of_hell1@mail.ru>
2026-01-05 18:50:31 +0300

    Update sessions.js

--- 15/src/routes/sessions.js
+++ 15/src/routes/sessions.js
@@ -4,6 +4,30 @@ export default (app) => {
   const users = generateUsers();
 
   // BEGIN (write your solution here)
+   app.get("/sessions/new", (req, res) => {
+    res.view("src/views/sessions/new", { error: null });
+  });
+
+  app.post("/sessions", (req, res) => {
+    const { username, password } = req.body;
+    
+    const user = users.find((u) => u.username === username);
+    
+    if (!user || decrypt(user.password) !== password) {
+      return res.view("src/views/sessions/new", { 
+        error: "Wrong username or password" 
+      });
+    }
+    
+    req.session.username = username;
+    return res.redirect("/");
+  });
+
+  app.post("/sessions/delete", (req, res) => {
+    req.session.destroy();
+    return res.redirect("/");
+  });
+
 
   // END
 };
