e074c7136966e63067958481383c57c3af9e4145
Udashkins <king_of_hell1@mail.ru>
2026-01-05 18:44:18 +0300

    Update server.js

--- 4/src/server.js
+++ 4/src/server.js
@@ -7,6 +7,17 @@ export default () => {
   const companies = getCompanies();
 
   // BEGIN (write your solution here)
+  app.get('/companies/:id', async (request, reply) => {
+    const companyId = request.params.id;
+    
+    const company = companies.find(company => company.id === companyId);
+    
+    if (!company) {
+      return reply.status(404).send('Company not found');
+    }
+    
+    return reply.send(company);
+  });
 
   // END
 
